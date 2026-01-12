56e521e1b88f3eb1f232384fb1bde829bc00b755
TracywLan <tracylan12@gmail.com>
2026-01-12 12:25:04 -0500

    complete basic sign-up page

---
+++ controllers/auth.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const User = require('../models/user') // import User model into this file
+const bcrypt = require("bcrypt");
+
+
+
+
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", (async(req, res)=>{  // controller for posting in sign-up
+    // check that username is unique
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    } 
+    // check passwords match
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up ${user.username}`);
+    
+}))
+
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs")
+})
+
+router.post("/sign-in", async (req, res) => {
+  // First, get the user from the database
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  // There is a user! Time to test their password with bcrypt
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  // There is a user AND they had the correct password. Time to make a session!
+  // Avoid storing the password, even in hashed format, in the session
+  // If there is other data you want to save to `req.session.user`, do so here!
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id
+  };
+
+  res.redirect("/");
+});
+
+router.get("/sign-out", (req, res) => {
+  res.send("The user wants out!");
+});
+
+
+module.exports = router;
+
+
